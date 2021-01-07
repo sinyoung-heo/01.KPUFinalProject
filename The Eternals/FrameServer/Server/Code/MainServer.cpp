0b2748ad4937bf8a2b85cfd66d8a5563fd2fb3c8
Lee Dong Hee <jjaing01@naver.com>
2021-01-07 21:12:53 +0900

    Creating Server Framework

--- The Eternals/FrameServer/Server/Code/MainServer.cpp
+++ The Eternals/FrameServer/Server/Code/MainServer.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#define TEST
 
 /* IOCP SERVER 관련 변수*/
 HANDLE g_hIocp;
@@ -19,7 +20,87 @@ SQLHSTMT g_hstmt;
 SQLRETURN g_retcode;
 SQLHENV g_henv;
 
+/*==============================================================함수 선언부========================================================================*/
+void Ready_Server();				// 서버 메인 루프 초기화
+
+void time_worker();					// Timer Thread Enter Function
+void worker_thread();				// Worker Thread Enter Function
+
+/*==============================================================MAIN CODE=========================================================================*/
 int main()
 {
+	Ready_Server();
+
+	/* Time Thread 생성 */
+	thread time_thread{ time_worker };
+
+	/* Worker Threads 생성 - 4개 */
+	vector<thread> vecWorker;
+	for (int i = 0; i < SERVER_CORE; ++i)
+		vecWorker.emplace_back(worker_thread);
+
+	/* Worker Threads 소멸 */
+	for (auto& th : vecWorker)
+		th.join();
+
+	/* Time Thread 소멸 */
+	time_thread.join();
+
+	closesocket(g_hListenSock);
+	WSACleanup();
+
 	return NO_EVENT;
+}
+
+/*==============================================================함수 정의부========================================================================*/
+void Ready_Server()
+{
+	std::wcout.imbue(std::locale("korean"));
+
+	/* WINSOCK 초기화 */
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	/* IOCP 생성 */
+	g_hIocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 0);
+
+	/* Listen Socket 생성 */
+	g_hListenSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+
+	/* Listen Socket -> IOCP 등록 */
+	CreateIoCompletionPort(reinterpret_cast<HANDLE>(g_hListenSock), g_hIocp, KEY_SERVER, 0);
+
+	/* Server Socket Addr 구조체 생성 */
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(SOCKADDR_IN));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(SERVER_PORT);
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+	/* bind */
+	::bind(g_hListenSock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+	/* listen */
+	listen(g_hListenSock, SOMAXCONN);
+
+	/* 통신용 Client Socket 생성*/
+	SOCKET cSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	g_accept_over.op_mode = OP_MODE_ACCEPT;
+	g_accept_over.wsa_buf.len = static_cast<int>(cSocket);
+	ZeroMemory(&g_accept_over.wsa_over, sizeof(&g_accept_over.wsa_over));
+
+	/* 비동기 Accept */
+	AcceptEx(g_hListenSock, cSocket, g_accept_over.iocp_buf, 0, 32, 32, NULL, &g_accept_over.wsa_over);
+}
+
+
+
+
+
+
+void time_worker()
+{
+}
+
+void worker_thread()
+{
 }
--- The Eternals/FrameServer/Server/Header/define.h
+++ The Eternals/FrameServer/Server/Header/define.h
@@ -11,6 +11,8 @@ constexpr int KEY_SERVER		= 1000000;
 #define DEAD_OBJ 1
 #define NO_INIT -1
 #define PI 3.141592
+constexpr int SERVER_CORE		= 4;
+
 
 /* GAME CONTENTS */
 constexpr int ZERO_HP			= 0;
