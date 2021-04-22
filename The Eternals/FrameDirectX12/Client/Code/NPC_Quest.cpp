907aaf8954a296ec07a3e8f1308e0dca698bc40b
Lee Dong Hee <jjaing01@naver.com>
2021-04-22 23:19:46 +0900

    Create Filter NPC_Quest & NPC_Children

---
+++ The Eternals/FrameDirectX12/Client/Code/NPC_Quest.cpp
@@ -0,0 +1,288 @@
+#include "stdafx.h"
+#include "NPC_Merchant.h"
+
+#include "GraphicDevice.h"
+#include "DirectInput.h"
+#include "ObjectMgr.h"
+#include "LightMgr.h"
+#include "Font.h"
+#include "RenderTarget.h"
+#include "TimeMgr.h"
+
+CNPC_Merchant::CNPC_Merchant(ID3D12Device* pGraphicDevice, ID3D12GraphicsCommandList* pCommandList)
+	: Engine::CGameObject(pGraphicDevice, pCommandList)
+	, m_pPacketMgr(CPacketMgr::Get_Instance())
+	, m_pServerMath(CServerMath::Get_Instance())
+{
+}
+
+HRESULT CNPC_Merchant::Ready_GameObject(wstring wstrMeshTag, wstring wstrNaviMeshTag, const _vec3& vScale, const _vec3& vAngle, const _vec3& vPos)
+{
+	Engine::FAILED_CHECK_RETURN(Engine::CGameObject::Ready_GameObject(true, true, true, true), E_FAIL);
+	Engine::FAILED_CHECK_RETURN(Add_Component(wstrMeshTag, wstrNaviMeshTag), E_FAIL);
+	m_pTransCom->m_vScale = vScale;
+	m_pTransCom->m_vAngle = vAngle;
+	m_pTransCom->m_vPos = vPos;
+	m_wstrCollisionTag = L"NPC_Merchant";
+
+	m_pNaviMeshCom->Set_CurrentCellIndex(m_pNaviMeshCom->Get_CurrentPositionCellIndex(vPos));
+	Engine::CGameObject::SetUp_BoundingBox(&(m_pTransCom->m_matWorld),
+		m_pTransCom->m_vScale,
+		m_pMeshCom->Get_CenterPos(),
+		m_pMeshCom->Get_MinVector(),
+		m_pMeshCom->Get_MaxVector());
+
+	Engine::CGameObject::SetUp_BoundingSphere(&(m_pTransCom->m_matWorld),
+											 m_pTransCom->m_vScale,
+											 _vec3(200.0f),
+											 _vec3(0.0f, 20.0f, 0.0f));
+	m_lstCollider.push_back(m_pBoundingSphereCom);
+
+
+	m_pInfoCom->m_fSpeed = 1.f;
+	m_bIsMoveStop = true;
+	m_fAniTime = 0.f;
+
+	/*__________________________________________________________________________________________________________
+	[ 애니메이션 설정 ]
+	____________________________________________________________________________________________________________*/
+	m_uiAnimIdx = 0;
+	m_iMonsterStatus = Baraka_M_Merchant::A_WAIT;
+
+	return S_OK;
+}
+
+HRESULT CNPC_Merchant::LateInit_GameObject()
+{
+	// SetUp Shader ConstantBuffer
+	m_pShaderCom->SetUp_ShaderConstantBuffer((_uint)(m_pMeshCom->Get_DiffTexture().size()));
+	m_pShadowCom->SetUp_ShaderConstantBuffer((_uint)(m_pMeshCom->Get_DiffTexture().size()));
+
+	return S_OK;
+}
+
+_int CNPC_Merchant::Update_GameObject(const _float& fTimeDelta)
+{
+	Engine::FAILED_CHECK_RETURN(Engine::CGameObject::LateInit_GameObject(), E_FAIL);
+
+	if (m_bIsDead)
+		return DEAD_OBJ;
+
+	// Angle Linear Interpolation
+	SetUp_AngleInterpolation(fTimeDelta);
+
+	/* Animation AI */
+	Change_Animation(fTimeDelta);
+
+	/*__________________________________________________________________________________________________________
+	[ Collision - Add Collision List ]
+	____________________________________________________________________________________________________________*/
+	m_pCollisonMgr->Add_CollisionCheckList(this);
+
+	/*__________________________________________________________________________________________________________
+	[ Play Animation ]
+	____________________________________________________________________________________________________________*/
+	m_pMeshCom->Set_AnimationKey(m_uiAnimIdx);
+	m_pMeshCom->Play_Animation(fTimeDelta * TPS);
+	m_ui3DMax_NumFrame = *(m_pMeshCom->Get_3DMaxNumFrame());
+	m_ui3DMax_CurFrame = *(m_pMeshCom->Get_3DMaxCurFrame());
+
+	/*__________________________________________________________________________________________________________
+	[ TransCom - Update WorldMatrix ]
+	____________________________________________________________________________________________________________*/
+	Engine::CGameObject::Update_GameObject(fTimeDelta);
+
+	return NO_EVENT;
+}
+
+_int CNPC_Merchant::LateUpdate_GameObject(const _float& fTimeDelta)
+{
+	Engine::NULL_CHECK_RETURN(m_pRenderer, -1);
+
+	/*__________________________________________________________________________________________________________
+	[ Renderer - Add Render Group ]
+	____________________________________________________________________________________________________________*/
+	Engine::FAILED_CHECK_RETURN(m_pRenderer->Add_Renderer(Engine::CRenderer::RENDER_NONALPHA, this), -1);
+
+	return NO_EVENT;
+}
+
+void CNPC_Merchant::Send_PacketToServer()
+{
+}
+
+void CNPC_Merchant::Render_GameObject(const _float& fTimeDelta, ID3D12GraphicsCommandList* pCommandList, const _int& iContextIdx)
+{
+	Set_ConstantTable();
+	m_pMeshCom->Render_DynamicMesh(pCommandList, iContextIdx, m_pShaderCom);
+}
+
+void CNPC_Merchant::Render_ShadowDepth(const _float& fTimeDelta, ID3D12GraphicsCommandList* pCommandList, const _int& iContextIdx)
+{
+	Set_ConstantTableShadowDepth();
+	m_pMeshCom->Render_DynamicMeshShadowDepth(pCommandList, iContextIdx, m_pShadowCom);
+}
+
+HRESULT CNPC_Merchant::Add_Component(wstring wstrMeshTag, wstring wstrNaviMeshTag)
+{
+	Engine::NULL_CHECK_RETURN(m_pComponentMgr, E_FAIL);
+
+	// Mesh
+	m_pMeshCom = static_cast<Engine::CMesh*>(m_pComponentMgr->Clone_Component(wstrMeshTag.c_str(), Engine::COMPONENTID::ID_STATIC));
+	Engine::NULL_CHECK_RETURN(m_pMeshCom, E_FAIL);
+	m_pMeshCom->AddRef();
+	m_mapComponent[Engine::ID_STATIC].emplace(L"Com_Mesh", m_pMeshCom);
+
+	// Shader
+	m_pShaderCom = static_cast<Engine::CShaderMesh*>(m_pComponentMgr->Clone_Component(L"ShaderMesh", Engine::COMPONENTID::ID_STATIC));
+	Engine::NULL_CHECK_RETURN(m_pShaderCom, E_FAIL);
+	m_pShaderCom->AddRef();
+	Engine::FAILED_CHECK_RETURN(m_pShaderCom->Set_PipelineStatePass(0), E_FAIL);
+	m_mapComponent[Engine::ID_STATIC].emplace(L"Com_Shader", m_pShaderCom);
+
+	// Shadow
+	m_pShadowCom = static_cast<Engine::CShaderShadow*>(m_pComponentMgr->Clone_Component(L"ShaderShadow", Engine::COMPONENTID::ID_STATIC));
+	Engine::NULL_CHECK_RETURN(m_pShadowCom, E_FAIL);
+	m_pShadowCom->AddRef();
+	Engine::FAILED_CHECK_RETURN(m_pShadowCom->Set_PipelineStatePass(0), E_FAIL);
+	m_mapComponent[Engine::ID_STATIC].emplace(L"Com_Shadow", m_pShadowCom);
+
+	// NaviMesh
+	m_pNaviMeshCom = static_cast<Engine::CNaviMesh*>(m_pComponentMgr->Clone_Component(wstrNaviMeshTag.c_str(), Engine::ID_DYNAMIC));
+	Engine::NULL_CHECK_RETURN(m_pNaviMeshCom, E_FAIL);
+	m_pNaviMeshCom->AddRef();
+	m_mapComponent[Engine::ID_STATIC].emplace(L"Com_NaviMesh", m_pNaviMeshCom);
+
+	return S_OK;
+}
+
+void CNPC_Merchant::Set_ConstantTable()
+{
+	/*__________________________________________________________________________________________________________
+	[ Set ConstantBuffer Data ]
+	____________________________________________________________________________________________________________*/
+	Engine::SHADOW_DESC tShadowDesc = CShadowLightMgr::Get_Instance()->Get_ShadowDesc();
+
+	Engine::CB_SHADER_MESH tCB_ShaderMesh;
+	ZeroMemory(&tCB_ShaderMesh, sizeof(Engine::CB_SHADER_MESH));
+	tCB_ShaderMesh.matWorld = Engine::CShader::Compute_MatrixTranspose(m_pTransCom->m_matWorld);
+	tCB_ShaderMesh.matLightView = Engine::CShader::Compute_MatrixTranspose(tShadowDesc.matLightView);
+	tCB_ShaderMesh.matLightProj = Engine::CShader::Compute_MatrixTranspose(tShadowDesc.matLightProj);
+	tCB_ShaderMesh.vLightPos = tShadowDesc.vLightPosition;
+	tCB_ShaderMesh.fLightPorjFar = tShadowDesc.fLightPorjFar;
+
+	m_fDeltaTime += (Engine::CTimerMgr::Get_Instance()->Get_TimeDelta(L"Timer_TimeDelta")) * 0.15f;
+	tCB_ShaderMesh.fDissolve = m_fDeltaTime;
+	m_pShaderCom->Get_UploadBuffer_ShaderMesh()->CopyData(0, tCB_ShaderMesh);
+
+	if (m_fDeltaTime > 1.f)
+		m_fDeltaTime = 0.f;
+}
+
+void CNPC_Merchant::Set_ConstantTableShadowDepth()
+{
+	/*__________________________________________________________________________________________________________
+	[ Set ConstantBuffer Data ]
+	____________________________________________________________________________________________________________*/
+	Engine::SHADOW_DESC tShadowDesc = CShadowLightMgr::Get_Instance()->Get_ShadowDesc();
+
+	Engine::CB_SHADER_SHADOW tCB_ShaderShadow;
+	ZeroMemory(&tCB_ShaderShadow, sizeof(Engine::CB_SHADER_SHADOW));
+	tCB_ShaderShadow.matWorld = Engine::CShader::Compute_MatrixTranspose(m_pTransCom->m_matWorld);
+	tCB_ShaderShadow.matView = Engine::CShader::Compute_MatrixTranspose(tShadowDesc.matLightView);
+	tCB_ShaderShadow.matProj = Engine::CShader::Compute_MatrixTranspose(tShadowDesc.matLightProj);
+	tCB_ShaderShadow.fProjFar = tShadowDesc.fLightPorjFar;
+
+	m_pShadowCom->Get_UploadBuffer_ShaderShadow()->CopyData(0, tCB_ShaderShadow);
+
+}
+
+void CNPC_Merchant::SetUp_AngleInterpolation(const _float& fTimeDelta)
+{
+	if (m_tAngleInterpolationDesc.is_start_interpolation)
+	{
+		m_tAngleInterpolationDesc.linear_ratio += m_tAngleInterpolationDesc.interpolation_speed * fTimeDelta;
+
+		m_pTransCom->m_vAngle.y = Engine::LinearInterpolation(m_tAngleInterpolationDesc.v1,
+			m_tAngleInterpolationDesc.v2,
+			m_tAngleInterpolationDesc.linear_ratio);
+
+		if (m_tAngleInterpolationDesc.linear_ratio == Engine::MAX_LINEAR_RATIO)
+		{
+			m_tAngleInterpolationDesc.is_start_interpolation = false;
+		}
+	}
+}
+
+void CNPC_Merchant::Change_Animation(const _float& fTimeDelta)
+{
+	
+
+	if (m_pMeshCom->Is_BlendingComplete())
+	{
+		switch (m_iMonsterStatus)
+		{
+
+		case Baraka_M_Merchant::A_WAIT:
+		{
+			m_fAniTime = 0.f;
+
+			m_uiAnimIdx = Baraka_M_Merchant::A_WAIT;
+			m_pMeshCom->Set_AnimationKey(m_uiAnimIdx);
+		}
+		break;
+
+		case Baraka_M_Merchant::A_GREET:
+		{
+			m_fAniTime += fTimeDelta;
+			m_uiAnimIdx = Baraka_M_Merchant::A_GREET;
+			m_pMeshCom->Set_AnimationKey(m_uiAnimIdx);
+
+			if (m_fAniTime > 10.f)
+			{
+				if (m_pMeshCom->Is_AnimationSetEnd(fTimeDelta))
+				{
+					m_iMonsterStatus = Baraka_M_Merchant::A_WAIT;
+
+					m_uiAnimIdx = Baraka_M_Merchant::A_WAIT;
+					m_pMeshCom->Set_AnimationKey(m_uiAnimIdx);
+
+					m_fAniTime = 0.f;
+				}
+			}
+		}
+		break;
+
+		case Baraka_M_Merchant::A_TALK:
+		{
+			m_fAniTime = 0.f;
+
+			m_uiAnimIdx = Baraka_M_Merchant::A_TALK;
+			m_pMeshCom->Set_AnimationKey(m_uiAnimIdx);
+		}
+		break;
+		}
+	}
+}
+
+Engine::CGameObject* CNPC_Merchant::Create(ID3D12Device* pGraphicDevice, ID3D12GraphicsCommandList* pCommandList, wstring wstrMeshTag, wstring wstrNaviMeshTag, const _vec3& vScale, const _vec3& vAngle, const _vec3& vPos)
+{
+	CNPC_Merchant* pInstance = new CNPC_Merchant(pGraphicDevice, pCommandList);
+
+	if (FAILED(pInstance->Ready_GameObject(wstrMeshTag, wstrNaviMeshTag, vScale, vAngle, vPos)))
+		Engine::Safe_Release(pInstance);
+
+	return pInstance;
+}
+
+void CNPC_Merchant::Free()
+{
+	Engine::CGameObject::Free();
+
+	Engine::Safe_Release(m_pMeshCom);
+	Engine::Safe_Release(m_pShaderCom);
+	Engine::Safe_Release(m_pShadowCom);
+	Engine::Safe_Release(m_pColliderSphereCom);
+	Engine::Safe_Release(m_pColliderBoxCom);
+	Engine::Safe_Release(m_pNaviMeshCom);
+}
