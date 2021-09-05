a2e6f80877b366ea494a78d33981fecc51942d01
jjaing01 <jjaing01@naver.com>
2021-09-05 21:38:16 +0900

    영상 촬영

--- The Eternals/FrameServer/Server/Header/protocol.h
+++ The Eternals/FrameServer/Server/Header/protocol.h
@@ -1,7 +1,7 @@
 #pragma once
 
 constexpr char SERVER_IP[16]			= "127.0.0.1";
-constexpr char SERVER_DHIP[16]			= "192.168.142.192";
+constexpr char SERVER_DHIP[16]			= "192.168.143.11";
 
 constexpr int SERVER_PORT				= 5689;
 
